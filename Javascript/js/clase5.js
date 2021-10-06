aba864df955c07b492217cc7db2d631e450318dc
Alfredo Guido Perosi <aperosi@yahoo.com>
2021-10-06 00:25:07 -0300

    2da version

---
+++ Javascript/js/clase5.js
@@ -0,0 +1,63 @@
+/*Arreglos */
+let elemento = document.querySelector("#principal");
+
+let mascotas = [];
+/*object 
+mascotas[1]="Loro"
+mascotas[0]="Perro"
+mascotas.push("Gallo");
+
+
+mascotas.forEach(m =>
+    elemento.innerHTML+= <h2>$ mascotas </h2> ;
+
+    //function crearform()   /*ESS */
+    const agregarMascota=()=>{
+        let datos = document.querySelectorAll("input");
+        let consulta
+        
+    }
+    let mascota = {
+        propietario: datos[0].value,
+        nombre: datos[1].value,
+        especie: datos[2].value,
+
+    }
+    });
+   /*ESG*/   
+   <form>
+       const crearform=()=>{
+           elemento.innerHTML="
+       }
+    <div class="mb-3">
+      <label for="propietario" class="form-label">Propiedad</label>
+      <input type="text" class="form-control" id="propietario" 
+    </div>
+    <div class="mb-3">
+      <label for="edad" class="form-control" id="edad" 
+       <input type="text" class="form-control" id="edad" 
+    </div>
+    <div class="mb-3">
+    <label for="peso" class="form-control" id="edad" 
+     <input type="text" class="form-control" id="peso" 
+  </div>
+  <div class="mb-3">
+  <label for="imagen" class="form-control" id="imagen" 
+   <input type="text" class="form-control" id="imagen" 
+</div>
+   
+   </div>
+    <button type="button" class="btn btn-primary">Agregar Datos</button>
+  </form>
+  ;
+
+crearform()=>{
+    elemento.innerHTML=`
+    <div class="card" style="width: 18rem;">
+    <img src="${m.imagen}" class="card-img-top" alt="...">
+    div class="card-body">
+        <h5 class="card-title">Nombre
+        <p class=
+
+    `
+}
